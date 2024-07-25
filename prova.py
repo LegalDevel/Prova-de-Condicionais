2f2e8f98d8905c829e5dd233657d6a0ded60d25b
Heliandro Aragão <157530163+LegalDevel@users.noreply.github.com>
2024-07-24 22:01:52 -0300

    Add files via upload

---
+++ prova.py
@@ -0,0 +1,10 @@
+def verificarNumero():
+    num = int(input("Digite um número: "))
+    if num > 0:
+        print(f"O número {num} é positivo!")
+    elif num < 0:
+        print(f"O número {num} é negativo!")
+    else:
+        print(f"O número {num} é nulo!")
+
+verificarNumero()
